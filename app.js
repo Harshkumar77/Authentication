b0244b0d3c9ea9b01166a72b74cb30e11ee21e26
mighty_volcano <harshkumar@noreply.com>
2021-11-22 23:48:26 +0530

    Refactoring the code

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-// Boiler plate
-
 const express = require("express");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then((_) => console.log("Connected to mongoDB"));
+/************************** Server Configs ************************************/
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 
-// Mongo scheme and models
+/************************** Mongo scheme and models ************************************/
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((_) => console.log("Connected to mongoDB"));
 
 const usersSchema = new mongoose.Schema({
   email: {
@@ -33,12 +30,9 @@ const usersSchema = new mongoose.Schema({
 
 const User = new mongoose.model("user", usersSchema);
 
-// Routes
+/************************** Routes ************************************/
 
-app.get("/", (req, res) =>
-  // ejs.renderFile(__dirname + "/ejs/home.ejs", (err, str) => res.send(str))
-  res.render("home")
-);
+app.get("/", (req, res) => res.render("home"));
 
 app
   .route("/register")
@@ -54,6 +48,7 @@ app
   })
   .post((req, res) => {});
 
+// Starting the server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
 });
